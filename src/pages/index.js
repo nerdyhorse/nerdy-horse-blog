32c7437d8c12ee399767cb94308106ba74e2f190
nerdyhorse <nerdyhorse.me@gmail.com>
2020-04-09 03:42:17 +0530

    ...

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,7 @@
 import React from "react"
-import { Link, graphql } from "gatsby"
+import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import { rhythm } from "../utils/typography"
 import HomeItemLayout from "../components/home-item-layout"
 
 const BlogIndex = ({ data, location }) => {
